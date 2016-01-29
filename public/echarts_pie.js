61ae2560bbdc2d26da0190ea5a89975711b1e2cc
Dongming <dongming0224@126.com>
2016-01-29 17:21:47 +0800

    add

--- public/echarts_pie.js
+++ public/echarts_pie.js
@@ -1,35 +1,34 @@
-//import echarts from 'echarts/lib/echarts';
-//import 'echarts/lib/chart/pie';
+// import echarts from 'echarts/lib/echarts';
+// import 'echarts/lib/chart/pie';
 
-define(function (require) {
-   // var VislibVisType = Private(require('ui/vislib_vis_type/VislibVisType'));
+define(function(require) {
+  // var VislibVisType = Private(require('ui/vislib_vis_type/VislibVisType'));
+  return function HistogramVisType(Private) {
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
     var Schemas = Private(require('ui/Vis/Schemas'));
     return new TemplateVisType({
-      name: 'echarts_pie', // The internal id of the visualization (must be unique)
+      name: 'echarts_pie', // The internal id of the visualization (must be
+                            // unique)
       title: 'Echarts Pie', // The title of the visualization, shown to the user
       description: 'Echarts Pie visualization', // The description of this vis
       icon: 'fa-cloud', // The font awesome icon of this visualization
-      template: require('plugins/kibana-plugin-echarts/echarts_pie.html'), // The template, that will be rendered for this visualization
+      template: require('plugins/kibana-plugin-echarts/echarts_pie.html'), 
       // Define the aggregation your visualization accepts
-      schemas: new Schemas([
-          {
-            group: 'metrics',
-            name: 'tagsize',
-            title: 'Tagsize',
-            min: 1,
-            max: 1,
-            aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
-          },
-          {
-            group: 'buckets',
-            name: 'tags',
-            title: 'Tags',
-            min: 1,
-            max: 1,
-            aggFilter: '!geohash_grid'
-          }
-        ])
+      schemas: new Schemas([{
+        group: 'metrics',
+        name: 'tagsize',
+        title: 'Tagsize',
+        min: 1,
+        max: 1,
+        aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
+      }, {
+        group: 'buckets',
+        name: 'tags',
+        title: 'Tags',
+        min: 1,
+        max: 1,
+        aggFilter: '!geohash_grid'
+      }])
     });
-
+  };
 });
