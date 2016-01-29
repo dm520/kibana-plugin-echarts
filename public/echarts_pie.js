2a41e1a5003225fec3f774dde1533390394f14e5
Dongming <dongming0224@126.com>
2016-01-29 16:53:35 +0800

    更新

--- public/echarts_pie.js
+++ public/echarts_pie.js
@@ -2,13 +2,9 @@
 //import 'echarts/lib/chart/pie';
 
 define(function (require) {
-
-
    // var VislibVisType = Private(require('ui/vislib_vis_type/VislibVisType'));
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
     var Schemas = Private(require('ui/Vis/Schemas'));
-
-    // Describe our visualization
     return new TemplateVisType({
       name: 'echarts_pie', // The internal id of the visualization (must be unique)
       title: 'Echarts Pie', // The title of the visualization, shown to the user
