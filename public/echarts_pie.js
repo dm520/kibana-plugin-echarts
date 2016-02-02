6536f513bd9e5e32dd48bf3c1c8b81640fe789f0
Dongming <dongming0224@126.com>
2016-02-02 12:08:50 +0800

    add

--- public/echarts_pie.js
+++ public/echarts_pie.js
@@ -4,31 +4,53 @@ define(function(require) {
   require('plugins/kibana-plugin-echarts/echartsPieController');
 
   // register the provider with the visTypes registry
- // require('ui/registry/vis_types').register(EchartsHistogramVisType);
+  // require('ui/registry/vis_types').register(EchartsHistogramVisType);
 
- return  function EchartsHistogramVisType(Private) {
+  return function EchartsHistogramVisType(Private) {
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
     var Schemas = Private(require('ui/Vis/Schemas'));
     return new TemplateVisType({
-      name: 'echarts_pie', // The internal id of the visualization
-      // (must be unique)
-      title: 'Echarts Pie', // The title of the visualization, shown to the user
-      description: 'Echarts Pie visualization', // The description of this vis
-      icon: 'fa-cloud', // The font awesome icon of this visualization
-      template: require('plugins/kibana-plugin-echarts/echarts_pie.html'),
-      // Define the aggregation your visualization accepts
+      name: 'echarts-pie',
+      title: 'Pie chart',
+      icon: 'fa-pie-chart',
+      description: 'Pie charts are ideal for displaying the parts of some whole. For example, sales percentages by department.' + 'Pro Tip: Pie charts are best used sparingly, and with no more than 7 slices per pie.',
+      params: {
+        defaults: {
+          shareYAxis: true,
+          addTooltip: true,
+          addLegend: true,
+          isDonut: false
+        },
+        editor: require('plugins/kbn_vislib_vis_types/editors/pie.html')
+      },
+      responseConverter: false,
+      hierarchicalData: true,
       schemas: new Schemas([{
         group: 'metrics',
-        name: 'tagsize',
-        title: 'Tagsize',
+        name: 'metric',
+        title: 'Slice Size',
         min: 1,
         max: 1,
-        aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
+        aggFilter: ['sum', 'count', 'cardinality'],
+        defaults: [{
+          schema: 'metric',
+          type: 'count'
+        }]
       }, {
         group: 'buckets',
-        name: 'tags',
-        title: 'Tags',
-        min: 1,
+        name: 'segment',
+        icon: 'fa fa-scissors',
+        title: 'Split Slices',
+        min: 0,
+        max: Infinity,
+        aggFilter: '!geohash_grid'
+      }, {
+        group: 'buckets',
+        name: 'split',
+        icon: 'fa fa-th',
+        title: 'Split Chart',
+        mustBeFirst: true,
+        min: 0,
         max: 1,
         aggFilter: '!geohash_grid'
       }])
